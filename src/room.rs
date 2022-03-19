f26a81456def216982227c36bcb2f81a943c0f49
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-03-19 03:16:34 -0400

    Implement RoomContext

--- src/room.rs
+++ src/room.rs
@@ -22,23 +22,54 @@ pub(crate) struct RoomContext {
     pub room_map: Arc<RwLock<HashMap<RoomID, Arc<Mutex<Room>>>>>,
 }
 
-pub(crate) fn room_setup(msg: String) -> Result<(RoomRequest, RoomID), serde_json::Error> {
-    match serde_json::from_str::<RoomRequest>(&msg) {
-        Ok(req) => match req {
+impl RoomContext {
+    pub(crate) fn new() -> RoomContext {
+        RoomContext::default()
+    }
+
+    fn get_room(&self, init_type: RoomRequest) -> Result<WrappedRoom, ()> {
+        let mut room_map = self.room_map.write().unwrap();
+        match init_type {
             RoomRequest::NewRoom => {
-                let room_id: RoomID = generate_room_name();
-                Ok((RoomRequest::NewRoom, room_id))
+                let mut room_name = generate_room_name();
+                let mut tries = 0;
+                while !room_map.contains_key(&room_name) || tries < 10 {
+                    log::debug!("Generated a room that was already taken! Try: {tries}");
+                    room_name = generate_room_name();
+                    tries += 1;
+                }
+                let room = Arc::new(Mutex::new(Room::new(room_name.clone())));
+                room_map.insert(room_name.clone(), room.clone());
+                Ok(room)
             }
-            // TODO: This isn't the smartest interface in the world
-            RoomRequest::JoinRoom(room) => Ok((RoomRequest::JoinRoom(room.clone()), room)),
-        },
-        Err(parse_err) => Err(parse_err),
+            RoomRequest::JoinRoom(room) => match room_map.get(&room) {
+                Some(room) => Ok(room.clone()),
+                None => return Err(()),
+            },
+        }
+    }
+
+    pub(crate) fn init_client(
+        &self,
+        client: RoomClient,
+        init_type: RoomRequest,
+    ) -> Result<Arc<std::sync::Mutex<Room>>, ()> {
+        let room: WrappedRoom = self.get_room(init_type)?;
+
+        // Add the client to the room and client maps
+        self.peer_map
+            .write()
+            .unwrap()
+            .insert(client.address, room.lock().unwrap().room_id.clone());
+        room.lock().unwrap().add_client(client).unwrap();
+        Ok(room) // Return the room
     }
 }
 
 // Represents a room a particular peer might be in
 pub(crate) struct Room {
     pub clients: HashMap<SocketAddr, Tx>,
+    pub room_id: String,
 }
 
 impl Room {
